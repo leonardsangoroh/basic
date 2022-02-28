514fbbf79e8dbced0df5048cd4307320e213a18e
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2022-02-28 16:08:02 +0300

    blade syntax

---
+++ resources/views/test.php
@@ -0,0 +1,24 @@
+<?php
+
+    //Lravel blade syntax example
+
+    $world = "Hi Leo";
+
+    echo $world;
+?>
+
+{{world}}
+
+if ($world) {
+
+} else {
+
+}
+
+@if()
+
+@endif
+
+@foreach
+
+@endforeach
