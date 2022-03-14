2a88214280c6e2d0bd7c607c26e87aecb3702ce9
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2022-03-14 10:36:58 +0300

    Make Serial No. column increment through the pages

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -37,10 +37,10 @@
                             </thead>
                             <tbody>
                                 
-                                @php($i = 1)
+                                <!-- @php($i = 1) -->
                                 @foreach($categories as $category)
                                     <tr>
-                                    <th scope="row">{{ $i++ }}</th>
+                                    <th scope="row">{{ $categories->firstItem()+$loop->index }}</th>
                                     <td>{{ $category->category_name }}</td>
                                     <td>{{ $category->user_id}}</td>
                                     <td>
